b94a2d86e87c93493a05b0cd0fca23fb9585cf6b
milihamster <hopemili@yahoo.com>
2020-04-04 12:41:06 +0200

    - Created UiLogin for debugging purposes
    - Created User Class to load from Database

--- PandaChat/src/de/pandastudios/chatengine/usermanagement/DatabaseHandler.java
+++ PandaChat/src/de/pandastudios/chatengine/usermanagement/DatabaseHandler.java
@@ -1,13 +1,41 @@
 package de.pandastudios.chatengine.usermanagement;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+import de.pandastudios.main.ServerMain;
+
 public class DatabaseHandler {
 
-	
-	
-	public void loadUser( String name, String passwort )
-	{
-		
+	public static void main(String[] args) {
+		DatabaseHandler handler = new DatabaseHandler();
 	}
-	
-	
+
+	public DatabaseHandler() {
+		UiLogin ui = new UiLogin();
+		ui.setVisible(true);
+
+		ui.getBtnLogin().addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				loadUser(ui.getTextName().getText(), ui.getTextPassword().getText());
+			}
+		});
+	}
+
+	public void loadUser(String name, String passwort) {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo", "root", "root");
+			// here sonoo is database name, root is username and password
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery("select * from emp");
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
 }
