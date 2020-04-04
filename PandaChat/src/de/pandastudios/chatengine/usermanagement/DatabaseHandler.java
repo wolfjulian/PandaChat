72ed988c4270f7ec8ec65fe7385f50fafc808429
milihamster <hopemili@yahoo.com>
2020-04-04 12:45:20 +0200

    - Added files to index

--- PandaChat/src/de/pandastudios/chatengine/usermanagement/DatabaseHandler.java
+++ PandaChat/src/de/pandastudios/chatengine/usermanagement/DatabaseHandler.java
@@ -26,12 +26,12 @@ public class DatabaseHandler {
 	public void loadUser(String name, String passwort) {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sonoo", "root", "root");
-			// here sonoo is database name, root is username and password
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pandachat", "root", "root");
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("select * from emp");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM user");
 			while (rs.next())
-				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+				System.out.println( rs.getInt(1) + rs.getString(2) );				
+				//System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
 			con.close();
 		} catch (Exception e) {
 			System.out.println(e);
---
+++ PandaChat/src/de/pandastudios/chatengine/usermanagement/UiLogin.java
@@ -0,0 +1,75 @@
+package de.pandastudios.chatengine.usermanagement;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class UiLogin extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textName;
+	private JTextField textPassword;
+	private JButton btnLogin;
+	private JLabel lblName;
+	private JLabel lblPassword;
+
+
+	public UiLogin() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 251, 137);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		contentPane.add(getTextName());
+		contentPane.add(getTextPassword());
+		contentPane.add(getBtnLogin());
+		contentPane.add(getLblName());
+		contentPane.add(getLblPassword());
+	}
+	protected JTextField getTextName() {
+		if (textName == null) {
+			textName = new JTextField();
+			textName.setBounds(93, 11, 139, 20);
+			textName.setColumns(10);
+		}
+		return textName;
+	}
+	protected JTextField getTextPassword() {
+		if (textPassword == null) {
+			textPassword = new JTextField();
+			textPassword.setBounds(93, 42, 139, 20);
+			textPassword.setColumns(10);
+		}
+		return textPassword;
+	}
+	protected JButton getBtnLogin() {
+		if (btnLogin == null) {
+			btnLogin = new JButton("Login");
+			btnLogin.setBounds(69, 73, 89, 23);
+		}
+		return btnLogin;
+	}
+	private JLabel getLblName() {
+		if (lblName == null) {
+			lblName = new JLabel("Name");
+			lblName.setBounds(10, 14, 46, 14);
+		}
+		return lblName;
+	}
+	private JLabel getLblPassword() {
+		if (lblPassword == null) {
+			lblPassword = new JLabel("Password");
+			lblPassword.setBounds(10, 45, 46, 14);
+		}
+		return lblPassword;
+	}
+}
