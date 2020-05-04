8468bbe9f60a138fe42edc31269e926bdd241e5f
milihamster <hopemili@yahoo.com>
2020-05-04 13:37:11 +0200

    - commited basically nothing substantial, just some updated DatabaseHandler stuff

--- PandaChat/src/de/pandastudios/chatengine/usermanagement/DatabaseHandler.java
+++ PandaChat/src/de/pandastudios/chatengine/usermanagement/DatabaseHandler.java
@@ -6,8 +6,11 @@ import java.sql.*;
 
 import de.pandastudios.main.ServerMain;
 
+
 public class DatabaseHandler {
 
+	// Zeile 1
+	
 	public static void main(String[] args) {
 		DatabaseHandler handler = new DatabaseHandler();
 	}
@@ -25,16 +28,21 @@ public class DatabaseHandler {
 
 	public void loadUser(String name, String passwort) {
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/pandachat", "root", "root");
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			Connection con = DriverManager.getConnection("jdbc:ucanaccess://D:/PandaChat.accdb");
+
+			//Statement geilesStatement = con.createStatement();
+			//geilesStatement.execute("INSERT INTO User (Benutzername, Chatname, Passwort, RegistriertSeit, GebanntBis, Moderator, Admin) "
+			//		+ "VALUES                         ( 'Schnitzel', 'GeilesSchnitzel', 'Schnitzel',  )");
+			
 			Statement stmt = con.createStatement();
-			ResultSet rs = stmt.executeQuery("SELECT * FROM user");
+			ResultSet rs = stmt.executeQuery("SELECT * FROM TestTabelle");
 			while (rs.next())
-				System.out.println( rs.getInt(1) + rs.getString(2) );				
-				//System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+				System.out.println( rs.getInt(1) + rs.getString(2) );
 			con.close();
 		} catch (Exception e) {
 			System.out.println(e);
+			System.out.print("DA GEHT WAS NISCH ):");
 		}
 	}
 
