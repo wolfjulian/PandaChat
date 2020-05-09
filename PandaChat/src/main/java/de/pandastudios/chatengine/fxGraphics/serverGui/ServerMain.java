1790dc9766f90f0eeac6a56a0155e73e5662c25b
Maik <heunisch@posteo.de>
2020-05-09 14:06:03 +0200

    - moved ClientsGui.fxml & ServerGui.fxml to resources

--- PandaChat/src/main/java/de/pandastudios/chatengine/fxGraphics/serverGui/ServerMain.java
+++ PandaChat/src/main/java/de/pandastudios/chatengine/fxGraphics/serverGui/ServerMain.java
@@ -6,12 +6,15 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.nio.file.Paths;
+
 public class ServerMain extends Application
 {
     @Override
     public void start ( Stage stage ) throws Exception
     {
-        Parent root = FXMLLoader.load(getClass().getResource("serverGui/ServerGui.fxml"));
+        System.out.println(Paths.get("/src/main/resources/ServerGui.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../../../../../../resources/ServerGui.fxml"));
         stage.setTitle("Server");
         stage.setScene(new Scene(root, 400, 400));
         stage.show();
